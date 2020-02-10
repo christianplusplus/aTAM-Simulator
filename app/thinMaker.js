df999e846f7dd4ff27bfa50df40109ddd2ba05e9
christianplusplus <48064080+christianplusplus@users.noreply.github.com>
2020-02-09 19:33:43 -0600

    Update thinMaker.js

--- app/thinMaker.js
+++ app/thinMaker.js
@@ -23,13 +23,13 @@ function makeThin(N, k)
     switch(c)
     {
         case 0:
-            seedStart0('<seed,col,'+d+',1>');
+            seedStart0('<seed|col|'+d+'|1>');
             break;
         case 1:
-            seedStart1('<seed,col,'+d+',1>');
+            seedStart1('<seed|col|'+d+'|1>');
             break;
         case 2:
-            seedStart2('<seed,col,'+d+',1>');
+            seedStart2('<seed|col|'+d+'|1>');
             break;
     }
     
@@ -37,30 +37,30 @@ function makeThin(N, k)
     {
         for(var j = 1; j <= 3*l-3; j++)
         {
-            upColumn('<seed,col,'+i+','+j+'>','<seed,col,'+i+','+(j+1)+'>');
+            upColumn('<seed|col|'+i+'|'+j+'>','<seed|col|'+i+'|'+(j+1)+'>');
         }
         if(ithFromRight(ithFromRight(s,i,m),l-1,2))
-            seedMsb1('<seed,col,'+i+','+(3*l-2)+'>','<seed,bit,'+i+','+(l-1)+'>');
+            seedMsb1('<seed|col|'+i+'|'+(3*l-2)+'>','<seed|bit|'+i+'|'+(l-1)+'>');
         else
-            seedMsb0('<seed,col,'+i+','+(3*l-2)+'>','<seed,bit,'+i+','+(l-1)+'>');
+            seedMsb0('<seed|col|'+i+'|'+(3*l-2)+'>','<seed|bit|'+i+'|'+(l-1)+'>');
         for(var j = 2; j <= l - 1; j++)
         {
             if(ithFromRight(ithFromRight(s,i,m),j-1,2))
-                seedBit1('<seed,bit,'+i+','+j+'>','<seed,bit,'+i+','+(j-1)+'>');
+                seedBit1('<seed|bit|'+i+'|'+j+'>','<seed|bit|'+i+'|'+(j-1)+'>');
             else
-                seedBit0('<seed,bit,'+i+','+j+'>','<seed,bit,'+i+','+(j-1)+'>');
+                seedBit0('<seed|bit|'+i+'|'+j+'>','<seed|bit|'+i+'|'+(j-1)+'>');
         }
     }
     
-    seedBit1('<seed,bit,'+d+','+1+'>','<seed,bit,'+d+','+0+'>');
+    seedBit1('<seed|bit|'+d+'|'+1+'>','<seed|bit|'+d+'|'+0+'>');
     
     for(var i = 1; i <= d - 1; i++)
     {
-        seedBit0('<seed,bit,'+i+','+1+'>','<seed,bit,'+i+','+0+'>');
-        seedSpacer('<seed,bit,'+(i+1)+','+0+'>','<seed,col,'+i+','+1+'>');
+        seedBit0('<seed|bit|'+i+'|'+1+'>','<seed|bit|'+i+'|'+0+'>');
+        seedSpacer('<seed|bit|'+(i+1)+'|'+0+'>','<seed|col|'+i+'|'+1+'>');
     }
     
-    seedEnd('<seed,bit,1,0>','<inc,read,1>','<inc,read,0>');
+    seedEnd('<seed|bit|1|0>','<inc|read|1>','<inc|read|0>');
     
     for(var i = 0; i <= l - 2; i++)
     {
@@ -68,41 +68,41 @@ function makeThin(N, k)
         {
             var u = bin(j, i);
             
-            counterRead0('<inc,read,0'+u+'>','<inc,read,10'+u+'>','<inc,read,00'+u+'>');
-            counterRead1('<inc,read,1'+u+'>','<inc,read,11'+u+'>','<inc,read,01'+u+'>');
-            counterRead0('<copy,read,0'+u+'>','<copy,read,10'+u+'>','<copy,read,00'+u+'>');
-            counterRead1('<copy,read,1'+u+'>','<copy,read,11'+u+'>','<copy,read,01'+u+'>');
+            counterRead0('<inc|read|0'+u+'>','<inc|read|10'+u+'>','<inc|read|00'+u+'>');
+            counterRead1('<inc|read|1'+u+'>','<inc|read|11'+u+'>','<inc|read|01'+u+'>');
+            counterRead0('<copy|read|0'+u+'>','<copy|read|10'+u+'>','<copy|read|00'+u+'>');
+            counterRead1('<copy|read|1'+u+'>','<copy|read|11'+u+'>','<copy|read|01'+u+'>');
         }
     }
     
     for(var i = 0; i <= 2*m - 1; i++)
     {
         if(ithFromRight(i,l,2))
-            counterReadMsb1('<copy,read,'+bin(i,l)+'>','<copy,write,'+bin(i,l)+'>','<d_fill>');
+            counterReadMsb1('<copy|read|'+bin(i,l)+'>','<copy|write|'+bin(i,l)+'>','<d_fill>');
         else
-            counterReadMsb0('<copy,read,'+bin(i,l)+'>','<copy,write,'+bin(i,l)+'>','<d_fill>');
+            counterReadMsb0('<copy|read|'+bin(i,l)+'>','<copy|write|'+bin(i,l)+'>','<d_fill>');
     }
     
     for(var i = 0; i <= 2*m - 3; i++)
     {
         if(ithFromRight(i,l,2))
-            counterReadMsb1('<inc,read,'+bin(i,l)+'>','<copy,write,'+bin(i+2,l)+'>','<d_fill>');
+            counterReadMsb1('<inc|read|'+bin(i,l)+'>','<copy|write|'+bin(i+2,l)+'>','<d_fill>');
         else
-            counterReadMsb0('<inc,read,'+bin(i,l)+'>','<copy,write,'+bin(i+2,l)+'>','<d_fill>');
+            counterReadMsb0('<inc|read|'+bin(i,l)+'>','<copy|write|'+bin(i+2,l)+'>','<d_fill>');
     }
     
-    counterReadMsb1('<inc,read,'+bin(2*m-2,l)+'>','<inc,write_all_0s,1>','<d_fill>');
+    counterReadMsb1('<inc|read|'+bin(2*m-2,l)+'>','<inc|write_all_0s|1>','<d_fill>');
     
     for(var i = 1; i <= l - 1; i++)
     {
-        counterWrite0('<inc,write_all_0s,'+i+'>','<inc,write_all_0s,'+(i+1)+'>');
+        counterWrite0('<inc|write_all_0s|'+i+'>','<inc|write_all_0s|'+(i+1)+'>');
     }
     
     for(var i = 0; i < Math.pow(2, l-1); i++)
     {
         var u = bin(i, l-1);
-        counterWrite0('<copy,write,'+u+'0>','<copy,write,'+u+'>');
-        counterWrite1('<copy,write,'+u+'1>','<msd,write,'+u+'>');
+        counterWrite0('<copy|write|'+u+'0>','<copy|write|'+u+'>');
+        counterWrite1('<copy|write|'+u+'1>','<msd|write|'+u+'>');
     }
     
     for(var i = 1; i <= l-2; i++)
@@ -111,70 +111,70 @@ function makeThin(N, k)
         {
             var u = bin(j, i);
             
-            counterWrite0('<copy,write,'+u+'0>','<copy,write,'+u+'>');
-            counterWrite1('<copy,write,'+u+'1>','<copy,write,'+u+'>');
-            counterWrite0('<msd,write,'+u+'0>','<msd,write,'+u+'>');
-            counterWrite1('<msd,write,'+u+'1>','<msd,write,'+u+'>');
+            counterWrite0('<copy|write|'+u+'0>','<copy|write|'+u+'>');
+            counterWrite1('<copy|write|'+u+'1>','<copy|write|'+u+'>');
+            counterWrite0('<msd|write|'+u+'0>','<msd|write|'+u+'>');
+            counterWrite1('<msd|write|'+u+'1>','<msd|write|'+u+'>');
         }
     }
     
-    counterWriteMsb0('<inc,write_all_0s,'+l+'>','<inc,down_z_0,1>');
-    counterWriteMsb0('<copy,write,0>','<copy,down_z_0,1>');
-    counterWriteMsb1('<copy,write,1>','<copy,down_z_0,1>');
-    counterWriteMsb0('<msd,write,0>','<msd,down_z_0,1>');
-    counterWriteMsb1('<msd,write,1>','<msd,down_z_0,1>');
+    counterWriteMsb0('<inc|write_all_0s|'+l+'>','<inc|down_z_0|1>');
+    counterWriteMsb0('<copy|write|0>','<copy|down_z_0|1>');
+    counterWriteMsb1('<copy|write|1>','<copy|down_z_0|1>');
+    counterWriteMsb0('<msd|write|0>','<msd|down_z_0|1>');
+    counterWriteMsb1('<msd|write|1>','<msd|down_z_0|1>');
     
     for(var i = 1; i <= 3*l-2; i++)
     {
-        downColumn('<inc,down_z_0,'+i+'>','<inc,down_z_0,'+(i+1)+'>');
-        downColumn('<copy,down_z_0,'+i+'>','<copy,down_z_0,'+(i+1)+'>');
+        downColumn('<inc|down_z_0|'+i+'>','<inc|down_z_0|'+(i+1)+'>');
+        downColumn('<copy|down_z_0|'+i+'>','<copy|down_z_0|'+(i+1)+'>');
     }
     for(var i = 1; i <= 3*l-3; i++)
-        downColumn('<msd,down_z_0,'+i+'>','<msd,down_z_0,'+(i+1)+'>');
+        downColumn('<msd|down_z_0|'+i+'>','<msd|down_z_0|'+(i+1)+'>');
     
-    counterReturnColumnStart('<inc,down_z_0,'+(3*l-1)+'>','<inc,down_z_1,'+1+'>');
-    counterReturnColumnStart('<copy,down_z_0,'+(3*l-1)+'>','<copy,down_z_1,'+1+'>');
+    counterReturnColumnStart('<inc|down_z_0|'+(3*l-1)+'>','<inc|down_z_1|'+1+'>');
+    counterReturnColumnStart('<copy|down_z_0|'+(3*l-1)+'>','<copy|down_z_1|'+1+'>');
     
     for(var i = 0; i <= l - 1; i++)
     {
-        counterReturnColumn('<inc,down_z_1,'+i+'>','<inc,down_z_1,'+(i+1)+'>');
-        counterReturnColumn('<copy,down_z_1,'+i+'>','<copy,down_z_1,'+(i+1)+'>');
+        counterReturnColumn('<inc|down_z_1|'+i+'>','<inc|down_z_1|'+(i+1)+'>');
+        counterReturnColumn('<copy|down_z_1|'+i+'>','<copy|down_z_1|'+(i+1)+'>');
     }
     
-    counterReturnColumnEnd('<inc,down_z_1,'+l+'>','<inc,read,1>','<inc,read,0>');
-    counterReturnColumnEnd('<copy,down_z_1,'+l+'>','<copy,read,1>','<copy,read,0>');
+    counterReturnColumnEnd('<inc|down_z_1|'+l+'>','<inc|read|1>','<inc|read|0>');
+    counterReturnColumnEnd('<copy|down_z_1|'+l+'>','<copy|read|1>','<copy|read|0>');
     
     if(d == 1)
     {
-        returnRowSingle('<msd,down_z_0,'+(3*l-2)+'>','<inc,read,1>','<inc,read,0>','<d_fill>');
+        returnRowSingle('<msd|down_z_0|'+(3*l-2)+'>','<inc|read|1>','<inc|read|0>','<d_fill>');
     }
     else
     {
-        returnRowStart('<msd,down_z_0,'+(3*l-2)+'>','<return,1>','<d_fill>');
+        returnRowStart('<msd|down_z_0|'+(3*l-2)+'>','<return|1>','<d_fill>');
         for(var i = 1; i <= d-2; i++)
-            returnRow('<return,'+i+'>','<return,'+(i+1)+'>');
-        returnRowEnd('<return,'+(d-1)+'>','<inc,read,1>','<inc,read,0>');
+            returnRow('<return|'+i+'>','<return|'+(i+1)+'>');
+        returnRowEnd('<return|'+(d-1)+'>','<inc|read|1>','<inc|read|0>');
     }
     
-    upColumn('<inc,read,'+bin(2*m-1,l)+'>','<roof,col,2>');
-    upColumn('<roof,col,2>','<roof,col,3>');
+    upColumn('<inc|read|'+bin(2*m-1,l)+'>','<roof|col|2>');
+    upColumn('<roof|col|2>','<roof|col|3>');
     
     for(var i = 3; i <= l+2; i++)
-        roofChimney('<roof,col,'+i+'>','<roof,col,'+(i+1)+'>','<roof,filler,1>');
+        roofChimney('<roof|col|'+i+'>','<roof|col|'+(i+1)+'>','<roof|filler|1>');
     
     for(var i = 1; i <= d-1; i++)
-        roofFiller('<roof,filler,'+i+'>','<roof,filler,'+(i+1)+'>');
+        roofFiller('<roof|filler|'+i+'>','<roof|filler|'+(i+1)+'>');
     
-    roofCap('<roof,col,'+(l+r+4)+'>','<roof,r_shingle,1>','<roof,l_shingle,1>');
+    roofCap('<roof|col|'+(l+r+4)+'>','<roof|r_shingle|1>','<roof|l_shingle|1>');
     
     for(var i = l+3; i <= l+r+3; i++)
-        upColumn('<roof,col,'+i+'>','<roof,col,'+(i+1)+'>');
+        upColumn('<roof|col|'+i+'>','<roof|col|'+(i+1)+'>');
     
     for(var i = 1; i <= c+2;i++)
-        roofLeftShingle('<roof,l_shingle,'+i+'>','<roof,l_shingle,'+(i+1)+'>','<d_fill>');
+        roofLeftShingle('<roof|l_shingle|'+i+'>','<roof|l_shingle|'+(i+1)+'>','<d_fill>');
     if(r > 0)
         for(var i = 1; i <= 3*d-3;i++)
-            roofRightShingle('<roof,r_shingle,'+i+'>','<roof,r_shingle,'+(i+1)+'>','<d_fill>');
+            roofRightShingle('<roof|r_shingle|'+i+'>','<roof|r_shingle|'+(i+1)+'>','<d_fill>');
         
     downColumn('<d_fill>','<d_fill>');
     
